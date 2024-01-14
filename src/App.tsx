36d055eb5cf4fdabe4ba3773ea0f5088e2a9fa7d
Teun Schaeken <you@example.com>
2024-01-14 18:21:35 +0100

    new version

--- src/App.tsx
+++ src/App.tsx
@@ -1,154 +1,55 @@
 import { useEffect, useState } from "react"
 
+import { Button } from "./@/components/ui/button"
 
-const useTime = () => {
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    const startTime = new Date()
-    const intervalId = setInterval(() => {
-      const dt = (new Date()).getTime() - startTime.getTime()
-      setTime(dt);
-    }, 10);
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-  return time;
-};
-
-function useOnMount<T>({ before, after }: { before: T, after: T }): T {
-  return useOnTime([
-    { time: 0, value: before },
-    { time: 10, value: after },
-  ])
+export function TypographyInlineCode({text}: {text: string}) {
+  return (
+    <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+      <span>
+        {text}
+      </span>
+    </code>
+  )
 }
 
-function useOnTime<T>(timeseries: { time: number, value: T }[]): T {
-  const [value, setValue] = useState<T>(timeseries[0].value)
-
-  const t = useTime()
-  useEffect(() => {
-    const timeValue = [...timeseries].reverse().find(({ time }) => t > time)
-    if (timeValue) {
-      setValue(timeValue.value)
-    }
-  }, [t])
-  return value
-}
-
-
-
-function HighlightedComponent({ text, times }: { text: string, times: { in: number, out: number } }) {
-  return <div className={`bg-blue-400 transition-all ease-in-out
-    ${useOnTime([
-    { time: 0, value: 'w-[0%]' },
-    { time: times.in, value: 'w-[100%] text-white' },
-    { time: times.out, value: 'w-[0%]' },
-  ])}
-  `}
-  >
-    {text}
-  </div>
-}
-
-type VerticalFadeInProps = {
-  start: string
-  duration: string
-  yOffset: string
-}
-function useVerticalFadeIn(props: VerticalFadeInProps): React.CSSProperties {
-  return {
-    transition: 'all',
-    transitionDelay: props.start,
-    transitionDuration: props.duration,
-    ...useOnMount<React.CSSProperties>({
-      before: { translate: `0 ${props.yOffset}`, opacity: 0 },
-      after: { translate: `0 0` }
-    })
-  }
-}
 export default function App() {
 
   return (
-    <div className="w-screen h-screen bg-neutral-50 overflow-hidden"  >
+    <div className="w-screen h-screen bg-white"  >
+
       <div className="flex h-full items-center ">
 
-        <div className="px-4 w-full flex flex-col gap-6 items-center justify-center">
+        <div className="absolute px-4 w-full flex flex-col gap-6 items-center justify-center">
           <div className={`
-            flex justify-center bg-blue-400 rounded-full w-64 h-64 
-            transition-all duration-500 delay-500 ease-out
-            ${useOnMount({ before: "-translate-y-20 opacity-0 scale-110", after: "" })}
-          `} />
+            relative flex justify-center rounded-full w-64 h-64 
+            `}
+          >
+            <img src='pf.jpg' className="absolute rounded-full" />
+          </div>
+
+
           <div className="flex flex-col">
             <div className="text-center text-[2.5rem] tracking-tight"
-              style={useVerticalFadeIn({ start: "0", duration: '1000ms', yOffset: '-100%' })}
             >
               Teun Schaeken
             </div>
             <div className="tracking-tight"
-              style={useVerticalFadeIn({ start: "500ms", duration: '1000ms', yOffset: '-100%' })}
             >
-              Software engineer specialized in Devops, Front-end development and Test Automation.
-              {/* Software engineer specialized in&nbsp;
-              {/* <HighlightedComponent /> */}
-              {/* <div className="inline-block"><HighlightedComponent text="Devops" times={{in: 2_000, out: 3_000}}/> </div>
-              ,&nbsp; 
-              <div className="inline-block whitespace-nowrap"><HighlightedComponent text="Front-End development" times={{in: 2_500, out: 3_500}}/> </div>
-              &nbsp;and&nbsp; 
-              <div className="inline-block whitespace-nowrap"><HighlightedComponent text="Test Automation" times={{in: 3_000, out: 4_000}}/> </div> */}
-              {/* <div className="inline-block"><HighlightedComponent text="Test Automation" times={{in: 4_000, out: 5_000}}/> </div>
-              . */}
-
+              Software engineer specialized in Devops, Automation, and Full-stack development.
             </div>
             <div>
 
-              <div className="mt-4 mb-4 flex justify-center"
-                style={useVerticalFadeIn({ start: "1000ms", duration: '1000ms', yOffset: '-25%' })}
+              <div className="mt-4 mb-4 flex items-center flex-col gap-2"
 
               >
 
-                <a className=" bg-black
-                  hover:text-white duration-500 
-          rounded-full hover:bg-blue-400
-          text-neutral-50 group inline-flex items-center 
-          px-4 lg:px-5 lg:py-4 md:px-6 py-3 text-sm md:text-base font-semibold 
-          cursor-pointer transition-colors">
-                  CONTACT ME
-                </a>
+                <Button variant="default" >Get in touch</Button>
 
+                {/* <TypographyInlineCode text="teunschaeken@gmail.com"/>
+                <TypographyInlineCode text="+316 23110261"/> */}
               </div>
             </div>
 
-
-
-            {/* <div className="flex w-full justify-center">
-
-              <a
-                className="cursor-pointer
-              rounded-md border text-white
-              border-t3-purple-200/20 bg-t3-purple-100/10 
-              px-2 py-2 text-sm transition-colors duration-300 
-              hover:border-t3-purple-300/50 hover:bg-t3-purple-100/20 
-           md:px-3 md:py-3 md:text-lg lg:px-5 lg:py-4 lg:text-xl"
-              >
-                <code> teun.schaeken@gmail.com</code>
-              </a>
-            </div> */}
-
-            {/* <div className="mt-6 flex w-full justify-center">
-
-              <a
-                className="cursor-pointer
-              rounded-md border text-white
-              border-t3-purple-200/20 bg-t3-purple-100/10 
-              px-2 py-2 text-sm transition-colors duration-300 
-              hover:border-t3-purple-300/50 hover:bg-t3-purple-100/20 
-           md:px-3 md:py-3 md:text-lg lg:px-5 lg:py-4 lg:text-xl"
-              >
-                <code> +316 23110261 </code>
-              </a>
-            </div> */}
 
           </div>
         </div>
